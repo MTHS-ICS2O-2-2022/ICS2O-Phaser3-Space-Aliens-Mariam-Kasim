57f4f7bc5ad468c6e38c92f91bee6f90b8e30997
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-04-22 19:56:20 +0000

    main

--- js/game.js
+++ js/game.js
@@ -1,45 +1,32 @@
-/* global Phaser */
+  /* global Phaser */
+  
+  // Copyright (c) 2023 Mariam Kasim All rights reserved
+  //
+  // Created by: Mariam Kasim
+  // Created on: Apr 2023
+  //This is the Phaser3 configuration file
 
-// Copyright (c) 2023 Mariam Kasim All rights reserved
-//
-// Created by: Mariam Kasim
-// Created on: Apr 2023
-//This is the Phaser3 configuration file
-
-//scene imports statement
-import splashScene from "./splashScene.js"
-
-//create new scenes
-const splashScene = new splashScene()
-
-/**
- * Start Phaser game
- */
-const config = {
+  /**
+  *Start Phaser Game
+  */
+    const config = {
   type: Phaser.AUTO,
   width: 1920,
-  height: 1080, 
+  height: 1080,
   physics: {
     default: 'arcade',
     arcade: {
       debug: true
     }
   },
-  //set the background color
+ //set the background color
   backgroundColor: 0x5f6e7a
   ,scale: {
     mode: Phaser.Scale.FIT,
-    // we place it in the middle of the page
+   // we place it in the middle of the page
     autoCenter: Phaser.Scale.CENTER_BOTH
   },
 }
 
-const game = new Phaser.Game(config)
-console.log(game)
-
-// load scenes
-// Note: Remember any "key" is global and CAN NOT be reused!
-game.scene.add("splashScene", splashScene)
-
-// the start scene
-game.scene.start("splashScene")
+  const game = new Phaser.Game(config)
+  console.log(game)
