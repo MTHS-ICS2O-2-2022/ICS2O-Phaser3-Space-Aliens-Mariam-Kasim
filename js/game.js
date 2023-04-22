6777e562b667ec8e9e9370093e10491ca80cd1d2
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-04-22 20:10:41 +0000

    main

--- js/game.js
+++ js/game.js
@@ -1,32 +1,45 @@
-  /* global Phaser */
-  
-  // Copyright (c) 2023 Mariam Kasim All rights reserved
-  //
-  // Created by: Mariam Kasim
-  // Created on: Apr 2023
-  //This is the Phaser3 configuration file
+/* global Phaser */
 
-  /**
-  *Start Phaser Game
-  */
-    const config = {
+// Copyright (c) 2023 Mariam Kasim All rights reserved
+//
+// Created by: Mariam Kasim
+// Created on: Apr 2023
+//This is the Phaser3 configuration file
+
+//scene imports statement
+import splashScene from "./splashScene.js"
+
+//create new scenes
+const splashScene = new splashScene()
+
+/**
+ * Start Phaser game
+ */
+const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-      debug: true
-    }
+      debug: true,
+    },
   },
- //set the background color
-  backgroundColor: 0x5f6e7a
-  ,scale: {
+  //set the background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
     mode: Phaser.Scale.FIT,
-   // we place it in the middle of the page
-    autoCenter: Phaser.Scale.CENTER_BOTH
+    // we place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH,
   },
 }
 
-  const game = new Phaser.Game(config)
-  console.log(game)
+const game = new Phaser.Game(config)
+console.log(game)
+
+// load scenes
+// Note: Remember any "key" is global and CAN NOT be reused!
+game.scene.add("splashScene", splashScene)
+
+// the start scene
+game.scene.start("splashScene")
