34995e8fbd10c796c5c99b98ff24515f60c65559
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-05-25 01:34:42 +0000

    main

--- js/gameScene.js
+++ js/gameScene.js
@@ -27,9 +27,11 @@ class GameScene extends Phaser.Scene {
   constructor () {
     super({ key: "gameScene" })
 
-    this.background = null
     this.ship = null
-    this.fireMissile = null
+    this.fireMissile = false
+    this.score = 0
+    this.scoreText = null
+    this.scoreTextStyle = { font: "65px Arial", fill: "#ffffff", align: "center" }
   }
 
   /**
@@ -68,6 +70,8 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
 
+    this.scoreText = this.add.text(10, 10, "Score: " + this.score.toString(), this.scoreTextStyle)
+
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
 
     //create group for missiles
@@ -82,6 +86,8 @@ class GameScene extends Phaser.Scene {
     alienCollide.destroy()
     missileCollide.destroy()
     this.sound.play("explosion")
+    this.score = this.score + 1
+    this.scoreText.setText("Score: " + this.score.toString())
     this.createAlien()
     this.createAlien()
   }.bind(this))
